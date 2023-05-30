28f01054aec64d51a1ad786edcb26463b8b6dd4e
Dario Presutti <dariopresde@protonmail.com>
2023-05-30 13:24:06 +0200

    make a standalone app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
---
+++ src/app/app.config.ts
@@ -0,0 +1,11 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { routes } from './app.routes';
+
+export const AppConfig: ApplicationConfig = {
+    providers: [
+        provideRouter(routes, withComponentInputBinding()),
+        provideHttpClient(),
+    ],
+};
