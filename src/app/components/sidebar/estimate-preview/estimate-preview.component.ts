e8128d85a0edc43abe8422cd6fec2de85f21bc64
Dario Presutti <dariopresde@protonmail.com>
2023-05-31 23:06:06 +0200

    create sidebar estimates list

--- src/app/components/sidebar/estimate-preview/estimate-preview.component.ts
+++ src/app/components/sidebar/estimate-preview/estimate-preview.component.ts
@@ -1,14 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Estimate } from 'src/app/interfaces/estimate.interface';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-estimate-preview',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './estimate-preview.component.html',
   styleUrls: ['./estimate-preview.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EstimatePreviewComponent {
-
+  @Input() estimate!: Estimate;
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Signal, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { Store } from '@ngrx/store';
+import { EstimateActions, selectEstimates } from 'src/app/app.store';
+import { Observable } from 'rxjs';
+import { Estimate } from 'src/app/interfaces/estimate.interface';
+import { EstimatePreviewComponent } from './estimate-preview/estimate-preview.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule,  MatButtonModule, MatIconModule],
+  imports: [CommonModule,  MatButtonModule, MatIconModule, EstimatePreviewComponent],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  private store = inject(Store);
+  public estimates: Signal<Estimate[]> = this.store.selectSignal(selectEstimates);
+
+  ngOnInit(): void {
+    this.store.dispatch(EstimateActions.addingEstimate({ estimate: { id: 1, name: 'Estimate 1', description: 'Estimate 1 description' } }));
+    this.store.dispatch(EstimateActions.addingEstimate({ estimate: { id: 2, name: 'Estimate 2', description: 'Estimate 2 description' } }));
+    console.log(this.estimates())
+  }
+
+}
