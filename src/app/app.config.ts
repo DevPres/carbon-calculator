5f9020f400012011b5698192294f842b2770c074
Dario Presutti <dariopresde@protonmail.com>
2023-06-01 18:13:39 +0200

    provide interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,11 +8,14 @@ import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideEffects } from '@ngrx/effects';
 import * as AppEffects from './app.effects';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { TokenInterceptor } from './interceptor/token.interceptor';
 
 export const AppConfig: ApplicationConfig = {
     providers: [
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([TokenInterceptor])
+    ),
     provideStore({ estimates: estimateReducer }),
     provideEffects(AppEffects),
     provideStoreDevtools(),
