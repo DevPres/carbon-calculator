4361b120f840dadb666a19caa39a8ed3fb37ce56
Dario Presutti <dariopresde@protonmail.com>
2023-06-02 22:46:39 +0200

    add console error if estimate passed is wrong

--- src/app/pages/estimates/components/calculator.abstract.ts
+++ src/app/pages/estimates/components/calculator.abstract.ts
@@ -1,8 +1,19 @@
-import { Signal } from '@angular/core';
-import { VehicleEstimate } from 'src/app/interfaces/app.interface';
+import { Directive, Input, Signal, WritableSignal, signal } from '@angular/core';
+import { CalculatorType, VehiclesEstimate } from 'src/app/interfaces/app.interface';
 
 
-
+@Directive()
 export abstract class CalculatorComponent {
-    public emissionEstimate!: Signal<VehicleEstimate>;
+    @Input() estimate!: VehiclesEstimate | null
+
+    abstract calculatorKey: CalculatorType
+    public calculatedEstimate: WritableSignal<VehiclesEstimate | null> = signal(null);
+
+    ngOnInit(): void {
+      if(this.estimate && this.estimate.type !== this.calculatorKey) {
+        console.error(`CalculatorComponent: Needed a ${this.calculatorKey} estimate, but got a ${this.estimate.type} estimate instead.`)
+      }
+    }
+
+    abstract syncEstimate(): void
 }
