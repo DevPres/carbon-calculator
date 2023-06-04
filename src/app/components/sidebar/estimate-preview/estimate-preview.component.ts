8fe22c87b2155851845f557b614ade2fa130b6d0
Dario Presutti <dariopresde@protonmail.com>
2023-06-04 13:05:04 +0200

    add dummy routing to trigger ngOnInit of estimate.component again

--- src/app/components/sidebar/estimate-preview/estimate-preview.component.ts
+++ src/app/components/sidebar/estimate-preview/estimate-preview.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TotalEstimate } from 'src/app/interfaces/app.interface';
 import { MatCardModule } from '@angular/material/card';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-estimate-preview',
@@ -14,8 +14,15 @@ import { RouterModule } from '@angular/router';
 })
 export class EstimatePreviewComponent {
   @Input() estimate!: TotalEstimate;
+  router = inject(Router)
 
   get link(): string {
     return `/estimates/${this.estimate.id}`;
+  }
+
+  onClick(): void {
+    this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+      this.router.navigate([this.link]);
+    });
   }
 }
