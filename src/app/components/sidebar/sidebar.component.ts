c3ec6a81568add170d0784d4ab32f43543a35ab9
Dario Presutti <dariopresde@protonmail.com>
2023-06-04 20:24:57 +0200

    add selected id

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { Store } from '@ngrx/store';
-import { AppActions, selectEstimates } from 'src/app/app.store';
+import { AppActions, selectEstimates, selectSelectedEstimateId } from 'src/app/app.store';
 import { Observable } from 'rxjs';
 import { TotalEstimate } from 'src/app/interfaces/app.interface';
 import { EstimatePreviewComponent } from './estimate-preview/estimate-preview.component';
 import { CreateEstimateDialogComponent } from '../create-estimate-dialog/create-estimate-dialog.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule,  MatButtonModule, MatIconModule, EstimatePreviewComponent, MatDialogModule],
+  imports: [CommonModule,  MatButtonModule, MatIconModule, EstimatePreviewComponent, MatDialogModule, MatListModule, MatDividerModule],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -21,10 +24,15 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 export class SidebarComponent {
   @Input() sidebarOpen!: WritableSignal<boolean>;
 
-  private store = inject(Store);
-  public estimates: Signal<TotalEstimate[]> = this.store.selectSignal(selectEstimates);
+  private readonly store = inject(Store);
+  private readonly dialog = inject(MatDialog);
+  private readonly route = inject(ActivatedRoute);
 
-  constructor(public dialog: MatDialog) {}
+  public estimates: Signal<TotalEstimate[]> = this.store.selectSignal(selectEstimates);
+  public selectedEstimateId: Signal<string | null> = this.store.selectSignal(selectSelectedEstimateId)
+
+  ngOnInit(): void {
+  }
 
   onCloseSidebar(): void {
     this.sidebarOpen.update(v => false);
